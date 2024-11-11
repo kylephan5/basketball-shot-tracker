3ae07d8b4f5769bd202294d14859a8dd84aad87d
kylephan <kyledphan@gmail.com>
2024-11-10 20:08:47 -0500

    change camera

--- detection.py
+++ detection.py
@@ -20,8 +20,7 @@ class ShotDetector:
         return frame
 
     def start(self):
-        cap = cv2.VideoCapture(0)
-
+        cap = cv2.VideoCapture(1)
         while cap.isOpened():
             ret, frame = cap.read()
             if not ret:
